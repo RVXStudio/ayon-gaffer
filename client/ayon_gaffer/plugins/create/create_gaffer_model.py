8ab5013287d5e6eef3ced466603ad5b033497860
Sveinbjorn J. Tryggvason <sveinbjorn@rvx.is>
2024-05-03 16:29:10 +0000

    add model publish for gaffer, exports in usd and abc

---
+++ client/ayon_gaffer/plugins/create/create_gaffer_model.py
@@ -0,0 +1,29 @@
+from ayon_gaffer.api import plugin
+
+import Gaffer
+import GafferScene
+
+
+class CreateGafferModel(plugin.GafferCreatorBase):
+    identifier = "io.ayon.creators.gaffer.model"
+    label = "Model"
+    product_type = "model"
+    description = "Scene writer to model"
+    icon = "gears"
+
+    def _create_node(self,
+                     product_name: str,
+                     pre_create_data: dict,
+                     script: Gaffer.ScriptNode) -> Gaffer.Node:
+        node = GafferScene.SceneWriter(product_name)
+        script.addChild(node)
+        if len(self.selected_nodes) > 1:
+            mnode = GafferScene.MergeScenes()
+            script.addChild(mnode)
+            for idx, snode in enumerate([n for n in self.selected_nodes]):
+                mnode["in"][f"in{idx}"].setInput(snode["out"])
+            node["in"].setInput(mnode["out"])
+        elif len(self.selected_nodes) == 1:
+            node["in"].setInput(self.selected_nodes[0]["out"])
+
+        return node
---
+++ client/ayon_gaffer/plugins/publish/extract_model_abc.py
@@ -0,0 +1,58 @@
+import os
+import re
+
+import pyblish.api
+
+from ayon_core.pipeline import publish
+
+
+class ExtractGafferModelAbc(
+    publish.Extractor,
+    publish.OpenPypePyblishPluginMixin
+):
+    """Export Gaffer Scene Writer"""
+
+    order = pyblish.api.ExtractorOrder
+    label = "Extract Gaffer Model ABC"
+    hosts = ["gaffer"]
+    families = ["model"]
+
+    def process(self, instance):
+
+        node = instance.data["transientData"]["node"]
+
+        staging_dir = self.staging_dir(instance)
+        filename = "{0}.abc".format(instance.name)
+        path = os.path.join(staging_dir, filename)
+
+        original_filepath = node["fileName"].getValue()
+
+        node["fileName"].setValue(path)
+
+        if "#" in path:
+            # Replace hash tokens (#) with frame number
+            context = node.scriptNode().context()
+            frame = context.getFrame()
+
+            def fn(match):
+                padding = len(match.group(0))
+                return str(frame).zfill(padding)
+
+            path = re.sub("(#+)", fn, path)
+
+        # Export node
+        # TODO: Support `executeSequence(frames: List[int])` to render sequence
+        node.execute()
+
+        # Add representation to instance
+        ext = os.path.splitext(path)[-1].strip(".")
+        representation = {
+            "name": ext,
+            "ext": ext,
+            "files": os.path.basename(path),
+            "stagingDir": os.path.dirname(path),
+        }
+        representations = instance.data.setdefault("representations", [])
+        representations.append(representation)
+
+        node["fileName"].setValue(original_filepath)
