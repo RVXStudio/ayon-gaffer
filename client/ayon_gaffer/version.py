1114a70da4984685ff872fd9407325fc0e62e96e
Sveinbjorn J. Tryggvason <sveinbjorn@rvx.is>
2024-10-03 16:53:07 +0000

    push version to 0.4.3

--- client/ayon_gaffer/version.py
+++ client/ayon_gaffer/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring AYON addon 'gaffer' version."""
-__version__ = "0.4.2"
+__version__ = "0.4.3"
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "gaffer"
 title = "Gaffer"
-version = "0.4.2"
+version = "0.4.3"
 
 client_dir = "ayon_gaffer"
 
