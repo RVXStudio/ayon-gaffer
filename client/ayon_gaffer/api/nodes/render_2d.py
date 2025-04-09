d9a31a12c2bb36e9575e747ffc244710c70f677b
pierrer <pierrer@rvx.is>
2025-04-09 09:37:10 +0000

    add local render callback

--- client/ayon_gaffer/api/nodes/render_2d.py
+++ client/ayon_gaffer/api/nodes/render_2d.py
@@ -1,40 +1,40 @@
-
-import imath
-import os
-
-import GafferDispatch
-import GafferImage
-import GafferScene
-import IECore
 import Gaffer
-from ayon_core.lib import Logger
-
-log = Logger.get_logger("ayon_gaffer.api.nodes.render_layer")
-
+import GafferImage
+import GafferUI
+import imath
 
 class RenderNode2D(Gaffer.Box):
     def __init__(self, name="2DRender"):
-        self.plug_signal = None
-
         Gaffer.Box.__init__(self, name)
 
-        self.addChild(GafferImage.ImagePlug("in", Gaffer.Plug.Direction.In, flags=Gaffer.Plug.Flags.Default | Gaffer.Plug.Flags.Dynamic))
+        # Add input plug for the image
+        self.addChild(
+            GafferImage.ImagePlug(
+                "in", Gaffer.Plug.Direction.In,
+                flags=Gaffer.Plug.Flags.Default | Gaffer.Plug.Flags.Dynamic
+            )
+        )
 
         image_writer = GafferImage.ImageWriter("ImageWriter")
         self.addChild(image_writer)
 
         image_writer["in"].setInput(self["in"])
-
-
+        self.addChild(Gaffer.StringPlug("localRender", defaultValue='',
+                                                         flags=Gaffer.Plug.Flags.Default | Gaffer.Plug.Flags.Dynamic, ))
+    def submit_local_render(self):
+        print("toto")
 
 Gaffer.Metadata.registerNode(
     RenderNode2D,
-    "description",
-    """
-    I designate a render layer
-    """,
-
-    "nodeGadget:color", imath.Color3f(0.3203125, 0.125, 0),
-
-    plugs={}
+    "description", "I designate a render layer.",
+    "nodeGadget:color", imath.Color3f(0.3203125, 0.125, 0.0),
+    plugs={
+        "localRender": [
+            "nodule:type", "",
+            "layout:section", "Settings",
+            "plugValueWidget:type", "GafferUI.ButtonPlugValueWidget",
+            'buttonPlugValueWidget:clicked', 'plug.node().submit_local_render()',
+            'label', 'Submit Render Local',
+        ]
+    }
 )
