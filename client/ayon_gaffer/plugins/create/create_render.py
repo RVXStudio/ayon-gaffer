3517ea5444e63026f203d374a67523dfcf4ebb92
pierrer <pierrer@rvx.is>
2025-04-16 11:46:41 +0000

    bring back create render

---
+++ client/ayon_gaffer/plugins/create/create_render.py
@@ -0,0 +1,34 @@
+from ayon_gaffer.api import plugin
+from ayon_core.lib import (
+    BoolDef
+)
+
+
+import Gaffer
+from ayon_gaffer.api.nodes import AyonPublishTask
+
+
+class CreateGafferRender(plugin.GafferRenderCreator):
+    identifier = "io.ayon.creators.gaffer.render"
+    deprecated_identifiers = ["io.openpype.creators.gaffer.render"]
+    label = "Render"
+    product_type = "render"
+    description = "Farm rendering"
+    icon = "fa5.film"
+
+    def get_instance_attr_defs(self):
+        return [
+            BoolDef(
+                "farm_rendering",
+                default=True,
+                label="Farm rendering"
+            )
+        ]
+
+    def _create_node(self,
+                     product_name: str,
+                     pre_create_data: dict,
+                     script: Gaffer.ScriptNode) -> Gaffer.Node:
+        node = AyonPublishTask(product_name)
+        script.addChild(node)
+        return node
