1794f988034b8941126d75859078712609655945
Sveinbjorn J. Tryggvason <sveinbjorn@rvx.is>
2025-04-29 15:41:35 +0000

    remove default udim profiles for imagereader

--- server/settings/loader_plugins.py
+++ server/settings/loader_plugins.py
@@ -212,11 +212,7 @@ DEFAULT_LOADER_PLUGINS_SETTINGS = {
         "enabled": True,
         "node_name_template": "{folder[name]}_{ext}",
         "plugs": [],
-        "udim_profiles": [{
-            "product_type": ["image"],
-            "product_name": ["^texture\."],
-            "use_udims": True
-        }]
+        "udim_profiles": []
     },
     "GafferLoadImageAiImage": {
         "enabled": True,
