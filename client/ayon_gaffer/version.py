305577b83c48a0517b64fe8a6868985e1def5fad
Sveinbjorn J. Tryggvason <sveinbjorn@rvx.is>
2025-07-10 11:09:40 +0000

    Updating dev version to [0.5.3-dev]

--- client/ayon_gaffer/version.py
+++ client/ayon_gaffer/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring AYON addon 'gaffer' version."""
-__version__ = "0.5.2"
+__version__ = "0.5.3-dev"
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "gaffer"
 title = "Gaffer"
-version = "0.5.2"
+version = "0.5.3-dev"
 
 client_dir = "ayon_gaffer"
 
