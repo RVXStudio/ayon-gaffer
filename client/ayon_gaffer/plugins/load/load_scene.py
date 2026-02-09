2565ac44ebe7e1a3efddecd485c3d1c8ef5597b6
Sveinbjorn J. Tryggvason <sveinbjorn@rvx.is>
2026-02-09 17:09:44 +0000

    allow loading of crowd products as usds

--- client/ayon_gaffer/plugins/load/load_scene.py
+++ client/ayon_gaffer/plugins/load/load_scene.py
@@ -22,7 +22,8 @@ class GafferLoadScene(ayon_gaffer.api.plugin.GafferLoaderBase):
         "usd",
         "look",
         "animation",
-        "layout"
+        "layout",
+        "crowd",
     ]
     representations = ["abc", "usd"]
 
