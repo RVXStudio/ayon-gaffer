a0d028970d0315463a0424477be6f2659f71d983
Sveinbjorn J. Tryggvason <sveinbjorn@rvx.is>
2026-02-11 14:59:07 +0000

    also make the update function search for a sequence

--- client/ayon_gaffer/plugins/load/load_vdb_to_arnold.py
+++ client/ayon_gaffer/plugins/load/load_vdb_to_arnold.py
@@ -30,12 +30,7 @@ class GafferLoadArnoldVDB(ayon_gaffer.api.plugin.GafferLoaderBase):
 
         path = self.filepath_from_context(context).replace("\\", "/")
         # let's check if it's a sequence
-        seq = ayon_gaffer.api.utils.get_pyseq_sequence(path)
-        if len(seq) > 1:
-            # ok more than one frame, let's make a hash padding
-            padding = seq._get_padding()
-            hash_padding = int(padding[1:-1])*"#"  # convert %04d to ####
-            path = seq.format(f"%D%h{hash_padding}%t")
+        path = self.convert_path_to_sequence(path)
         node["fileName"].setValue(path)
         script.addChild(node)
 
@@ -54,6 +49,7 @@ class GafferLoadArnoldVDB(ayon_gaffer.api.plugin.GafferLoaderBase):
         representation = context["representation"]
         path = get_representation_path(representation)
         path = path.replace("\\", "/")
+        path = self.convert_path_to_sequence(path)
 
         node = container["_node"]
         node["fileName"].setValue(path)
@@ -66,6 +62,16 @@ class GafferLoadArnoldVDB(ayon_gaffer.api.plugin.GafferLoaderBase):
 
         parent = node.parent()
         parent.removeChild(node)
+
+    def convert_path_to_sequence(self, path):
+        seq = ayon_gaffer.api.utils.get_pyseq_sequence(path)
+        if len(seq) > 1:
+            # ok more than one frame, let's make a hash padding
+            padding = seq._get_padding()
+            hash_padding = int(padding[1:-1])*"#"  # convert %04d to ####
+            path = seq.format(f"%D%h{hash_padding}%t")
+        return path
+
 
 
 if not os.environ.get("ARNOLD_ROOT"):
