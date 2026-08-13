cbc504a24a2778024c09f8927be93e7edc261385
Sveinbjorn J. Tryggvason <sveinbjorn@rvx.is>
2026-08-13 09:31:18 +0000

    Updating version to [0.5.12]

--- client/ayon_gaffer/version.py
+++ client/ayon_gaffer/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring AYON addon 'gaffer' version."""
-__version__ = "0.5.12-dev"
+__version__ = "0.5.12"
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "gaffer"
 title = "Gaffer"
-version = "0.5.12-dev"
+version = "0.5.12"
 
 client_dir = "ayon_gaffer"
 
